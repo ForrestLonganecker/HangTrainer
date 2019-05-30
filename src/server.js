ba1e2aae0d41b447cead64f28b65c1a2a1075916
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-05-30 10:03:50 -0700

    added normalize port functionality

--- src/server.js
+++ src/server.js
@@ -1,10 +1,22 @@
 const app = require('./app');
 const http = require('http');
 const server = http.createServer(app);
-const port = 3000;
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
 server.listen(port);
 
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+  if(isNaN(port)) {
+    return val;
+  }
+  if(port >= 0) {
+    return port;
+  }
+  return false;
+}
+
 server.on('listening', () => {
-  console.log(`server is listening for requests on port: ${port}`);
+  console.log(`server is listening for requests on port: ${server.address().port}`);
 });
