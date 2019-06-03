a870e472201e4493c046da74ae7fdc8859069508
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-06-03 16:43:47 -0700

    express is sending response objects

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/workouts', (req, res) => {
-  return res.end('Work it!!')
+  return res.end('Work it!! from the express server!')
 });
 
 module.exports = router;
