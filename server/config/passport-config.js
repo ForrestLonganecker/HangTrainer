ee47fbaf8ef315015f84a4cfb2020e06a594661d
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-06-05 11:09:33 -0700

    added express.session, body-parser, passportConfig(using local strategy), created "currentUser" variableto store user object in res.locals.currentUser

---
+++ server/config/passport-config.js
@@ -0,0 +1,37 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-auth0');
+const User = require('../db/models').User;
+const authHelper = require('../auth/helpers');
+
+module.exports = {
+  init(server){
+    server.use(passport.initialize());
+    server.use(passport.session());
+
+    passport.use(new LocalStrategy({
+      usernameField: 'email'
+    }, (email, password, done) => {
+      User.findOne({where: {email}})
+      .then((user) => {
+        if(!user || !authHelper.comparePass(password, user.password)) {
+          return done(null, false, {message: 'Invalid email or password'});
+        }
+        return done(null, user);
+      })
+    }));
+
+    passport.serializeUser((user, callback) => {
+      callback(null, user.id);
+    });
+
+    passport.deserializeUser((id, callback) => {
+      User.findByPk(id)
+      .then((user) => {
+        callback(null, user);
+      })
+      .catch((err) => {
+        callback(err, user);
+      })
+    });
+  }
+}
--- server/config/server-config.js
+++ server/config/server-config.js
@@ -1,9 +1,24 @@
 // used to set up express middleware for the server-side app
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const passportConfig = require('../config/passport-config');
 
 module.exports = {
   init(server){
-    const logger = require('morgan');
-
     server.use(logger('dev'));
+    server.use(session({
+      secret: process.env.cookieSecret,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 1.21e+9 }
+    }));
+    server.use(bodyParser.urlencoded({ extended: true }));
+    passportConfig.init(server);
+    server.use((req, res, next) => {
+      res.locals.currentUser = req.user;
+      next();
+    })
   }
 }
