c2d80602d470ff931ee21b5888bb679713bc8b03
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-06-12 11:24:57 -0700

    Workout model is being created as expected

--- spec/unit/workout_spec.js
+++ spec/unit/workout_spec.js
@@ -1,12 +1,26 @@
 const sequelize = require('../../server/db/models/index').sequelize;
 const Workout = require('../../server/db/models').Workout;
+const User = require('../../server/db/models').User;
 
 describe('Workout', () => {
 
   beforeEach((done) => {
     sequelize.sync({force: true})
     .then(() => {
-      done();
+      this.user;
+
+      User.create({
+        email: 'rock@climb.com',
+        password: '123456'
+      })
+      .then((user) => {
+        this.user = user;
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -18,11 +32,13 @@ describe('Workout', () => {
     it('should create a workout object with assigned name, and notes', (done) => {
       Workout.create({
         name: 'Easy',
-        notes: 'For high-gravity days'
+        notes: 'For high-gravity days',
+        userId: this.user.id
       })
       .then((workout) => {
         expect(workout.name).toBe('Easy');
         expect(workout.notes).toBe('For high-gravity days');
+        expect(workout.userId).toBe(this.user.id);
         done();
       })
       .catch((err) => {
