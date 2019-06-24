0e1601649ac728a2c37a72fa535c60107a098023
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-06-23 17:02:35 -0700

    automatic reroute to landing if you are logged in and navigate to sign-up page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import SignIn from '../components/SignIn/SignIn';
 import SignUp from '../components/SignUp/SignUp';
 
 import '../scss/styles.scss';
+import Router from 'next/router';
 
 // need to make a layout that allows for me to put error message component in
 // the right spot
@@ -11,6 +13,18 @@ import '../scss/styles.scss';
 const Index = () => {
 
   const [currentDisplay, setCurrentDisplay] = useState('sign in');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+
+  useEffect(() => {
+    axios.get('/static/index')
+    .then(res => {
+      if(res.data){
+        setIsAuthenticated(true);
+        Router.push('/landing');
+      }
+    });
+  }, [isAuthenticated]);
 
   const handleDisplay = () => {
     if(currentDisplay === 'sign in'){
